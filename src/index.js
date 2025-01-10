2da4d043f6b5041850996ffd013b21f7a70d169b
Brandon Bakus <brandonbakus@gmail.com>
2025-01-10 16:52:04 -0500

    DONE

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTaskFormElement = document.getElementById('create-task-form')
+  createTaskFormElement.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const newTaskDescriptionInputElement = document.getElementById('new-task-description')
+    
+    const liElement = document.createElement('li')
+    liElement.textContent = newTaskDescriptionInputElement.value
+
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = "X"
+    deleteButton.setAttribute('data-description', newTaskDescriptionInputElement.value)
+    deleteButton.addEventListener('click', (event) => {
+      // liElement.remove()
+      event.target.parentNode.remove()
+    })
+
+    liElement.appendChild(deleteButton)
+    
+    const tasksUlElement = document.getElementById('tasks')
+    tasksUlElement.appendChild(liElement)
+
+    createTaskFormElement.reset()
+  })
 });
+
